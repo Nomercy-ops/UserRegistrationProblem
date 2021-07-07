f462be646178f9766142a38d74f8c18aecd73bbd
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-07-07 07:57:26 +0530

    [Rikesh]Refactor: add docstring for valid and invalid input test

--- Test/test_user_validation.py
+++ Test/test_user_validation.py
@@ -1,3 +1,11 @@
+"""
+@Author: Rikesh Chhetri
+@Date: 2021-07-01 
+@Last Modified by: Rikesh Chhetri
+@Last Modified time: 2021-07-01 10:03:30
+@Title : Test case for user registration problem.
+"""
+
 from UserValidation import ValidateUser as validate
 import unittest
 
@@ -10,101 +18,26 @@ class TestUserValidation(unittest.TestCase):
         """
     Description:
         The given valid name should return true in test case
-    
+
     Parameter:
         It takes self as a parameter.
-       
-    """
-        name = validate.ValidateName("Rikesh")
-        self.assertTrue(name)
 
-    def test_givenName_withMinimumThreeLetters_shouldReturnTrue(self):
-        
+    """
+
+        self.assertTrue(validate.ValidateName("Rikesh"))
+        self.assertTrue(validate.ValidateName("Rik"))
+
+    def test_givenInvalidName_shouldReturnFalse(self):
         """
     Description:
-        The given valid name with minimum 3 character
-        should return true in test case
-    
+        The given invalid name should return false in test case
+
     Parameter:
         It takes self as a parameter.
-       
+
     """
-        name = validate.ValidateName("Rik")
-        self.assertTrue(name)
-
-    def test_givenName_withLessThanThreeLetters_shouldReturnFalse(self):
-        
-        """
-    Description:
-        The given invalid name with less than three character
-        should return false in test case
-    
-    Parameter:
-        It takes self as a parameter.
-       
-    """
-        name = validate.ValidateName("Ri")
-        self.assertFalse(name)
-
-    def test_givenInvalidName_WithSmallLetter_shouldReturnfalse(self):
-        
-        """
-    Description:
-        The given invalid name with all small letter
-        should return false in test case
-    
-    Parameter:
-        It takes self as a parameter.
-       
-    """
-        name = validate.ValidateName("rikesh")
-        self.assertFalse(name)
-
-    def test_givenName_withAllUpperCaseCharacter_shouldReturnFalse(self):
-        
-        """
-    Description:
-        The given invalid name with all Capital letter
-        should return false in test case
-    
-    Parameter:
-        It takes self as a parameter.
-       
-    """
-        name = validate.ValidateName("RIKESH")
-        self.assertFalse(name)
-
-    def test_givenName_whenContainNumber_shouldReturnFalse(self):
-        
-        """
-    Description:
-        The given invalid name with numbers
-        should return false in test case
-    
-    Parameter:
-        It takes self as a parameter.
-       
-    """
-        name = validate.ValidateName("Ri786es")
-        self.assertFalse(name)
-
-    def test_givenName_whenContainSpecialCharacter_shouldReturnFalse(self):
-        
-        """
-    Description:
-        The given invalid name with special characters
-        should return false in test case
-    
-    Parameter:
-        It takes self as a parameter.
-       
-    """
-        name = validate.ValidateName("Ri@kesh")
-        self.assertFalse(name)
-
-
-            
-    #   " Test Case For Email "
-
-
-
+        self.assertFalse(validate.ValidateName("Ri"))
+        self.assertFalse(validate.ValidateName("rikesh"))
+        self.assertFalse(validate.ValidateName("RIKESH"))
+        self.assertFalse(validate.ValidateName("Ri786es"))
+        self.assertFalse(validate.ValidateName("Ri@kesh"))
